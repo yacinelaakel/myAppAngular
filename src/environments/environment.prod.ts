c5f73e679ff3974395642743d587c5bba5c546e0
YLaakel <yacine.laakel@hotmail.fr>
2018-12-05 19:59:53 +0100

    changed url production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   	production: true,
 	baseHref: '/',
-	apiUrl: 'http://localhost:8081/api',
+	apiUrl: 'https://yacine-mean-app.herokuapp.com/api',
 	vapidPublic: 'BCarQXBH_qirMS41tBG5KNFE9t1VoMB_LfsqfTYeScWtnULGeIbxkQsBpkLi8WjyAtlZT0EHymAQz1nZj2AKi14',
 	stripeKey: 'pk_test_9G3NuCpZNY9KXgr4GsLjw4mI'
 };
