b04fe15f311374968f99e3bb37ddf614ef87ea7e
YLaakel <yacine.laakel@hotmail.fr>
2018-12-06 11:26:07 +0100

    .htaccess

--- backend/api/utils/date_manager.js
+++ backend/api/utils/date_manager.js
@@ -1,13 +1,14 @@
 module.exports = {
 	timeStampToDate: function(timestamp) {
 		const date    = new Date(timestamp * 1000);
-		const day     = date.getDate();
+		var day       = date.getDate();
+		day   	      = '0' + day;
 		var month     = date.getMonth() + 1;
 		month   	  = '0' + month;
 		const year    = date.getFullYear();
 		const hours   = date.getHours();
 		const minutes = '0' + date.getMinutes();
 
-		return day + '-' + month.substr(-2) + '-' + year + ' ' + hours + ':' + minutes.substr(-2);
+		return day.substr(-2) + '-' + month.substr(-2) + '-' + year + ' ' + hours + ':' + minutes.substr(-2);
 	}
 }
